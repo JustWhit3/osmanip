6b1866d91e305d2be36868f9e597f2837cd34861
Gianluca Bianco <gianluca.bianco3@studio.unibo.it>
2021-11-13 15:53:52 +0100

    Added namespace.

--- include/osmanip.h
+++ include/osmanip.h
@@ -5,60 +5,63 @@
 #include <map>
 #include <chrono>
 
-//Maps declaration:
-extern std::map <std::string, std::string> col, sty, rst, crs;
-
-//Functions declaration:
-std::string feat ( std::map <std::string, std::string> & generic_map, std::string feat_string ),
-            reset ( std::string reset_string );
-
-//Test functions declaration:
-void feat_test( std::map <std::string, std::string> & generic_map_test ),
-     reset_test();
-     
-//ProgressBar class declaration:
-class ProgressBar
+namespace osm
  {
-  public:
+  //Maps declaration:
+  extern std::map <std::string, std::string> col, sty, rst, crs;
 
-   //Constructors and destructors declaration:
-   ProgressBar();
-   ~ProgressBar();
+  //Functions declaration:
+  std::string feat ( std::map <std::string, std::string> & generic_map, std::string feat_string ),
+              reset ( std::string reset_string );
 
-   //Setters declaration:
-   void setMax( long long int max ),
-        setMin( long long int min ),
-        setStyle( std::string style ),
-        setMessage( std::string message ),
-        setBegin(),
-        setEnd(),
+  //Test functions declaration:
+  void feat_test( std::map <std::string, std::string> & generic_map_test ),
+       reset_test();
+     
+  //ProgressBar class declaration:
+  class ProgressBar
+   {
+    public:
 
-   //Resetters declaration:
-        reset(),
-        resetMax(),
-        resetMin(),
-        resetStyle(),
-        resetMessage(),
-        resetTime();
+     //Constructors and destructors declaration:
+     ProgressBar();
+     ~ProgressBar();
 
-   //Getters declaration:
-   long long int getMax() const,
-                 getMin() const,
-                 getTime() const,
-                 getIteratingVar() const;
-   std::string getStyle() const,
-               getMessage() const;
+     //Setters declaration:
+     void setMax( long long int max ),
+          setMin( long long int min ),
+          setStyle( std::string style ),
+          setMessage( std::string message ),
+          setBegin(),
+          setEnd(),
 
-   //Other methods declaration:
-   void update( long long int iterating_var );
+     //Resetters declaration:
+          reset(),
+          resetMax(),
+          resetMin(),
+          resetStyle(),
+          resetMessage(),
+          resetTime();
+
+     //Getters declaration:
+     long long int getMax() const,
+                   getMin() const,
+                   getTime() const,
+                   getIteratingVar() const;
+     std::string getStyle() const,
+                 getMessage() const;
+
+     //Other methods declaration:
+     void update( long long int iterating_var );
   
-  private:
+    private:
 
-   //Attributes declaration and initializations:
-   long long int max_, min_, iterating_var_, time_count_;
-   std::string style_, conct_, message_;
-   static const std::string error_;
-   std::chrono::steady_clock::time_point begin, end;
- };
+     //Attributes declaration and initializations:
+     long long int max_, min_, iterating_var_, time_count_;
+     std::string style_, conct_, message_;
+     static const std::string error_;
+     std::chrono::steady_clock::time_point begin, end;
+   };
+ }
       
 #endif
