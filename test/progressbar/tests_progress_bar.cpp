1b5dc3a659c4d997c6814bf2fb113caac49fe7fe
JustWhit3 <gianluca.bianco3@studio.unibo.it>
2022-06-21 15:33:49 +0200

    Test.

--- test/progressbar/tests_progress_bar.cpp
+++ test/progressbar/tests_progress_bar.cpp
@@ -1,6 +1,7 @@
 //====================================================
 //     Preprocessor settings
 //====================================================
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #define DOCTEST_CONFIG_SUPER_FAST_ASSERTS
 
 //====================================================
