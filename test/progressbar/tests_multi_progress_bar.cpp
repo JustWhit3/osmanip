1fdc398439c111b5bd4846d2273f40b1f5f33c57
JustWhit3 <gianluca.bianco3@studio.unibo.it>
2022-06-22 18:02:07 +0200

    Corrected an error for which the tests compilation on Windows thrown "std::out_of_range".

--- test/progressbar/tests_multi_progress_bar.cpp
+++ test/progressbar/tests_multi_progress_bar.cpp
@@ -18,76 +18,83 @@
 #include <sstream>
 
 //====================================================
-//     Global variables
+//     MultiProgressBar class
 //====================================================
-osm::ProgressBar <int> bar1;
-osm::ProgressBar <double> bar2;
-osm::ProgressBar <float> bar3;
-auto bars = osm::MultiProgressBar( bar1, bar2, bar3 );
-
-//====================================================
-//     Testing "size" method
-//====================================================
-TEST_CASE( "Testing the size method" )
+TEST_CASE( "Testing MultiProgressBar class" )
  {
-  CHECK_EQ( bars.size(), 3 );
- }
-
-//====================================================
-//     Testing "operator()" redefinition
-//====================================================
-TEST_CASE( "Testing the operator() redefinition in the updater struct" )
- {
-  bar1.setMin( 0 );
-  bar1.setMax ( 60 );
-  bar1.setStyle( "indicator", "%" );
-
-  static std::stringstream ss_multi, ss_normal;
-
-  for( int i = bar1.getMin(); i < bar1.getMax(); i++ ) 
+  osm::ProgressBar <int> bar1;
+  osm::ProgressBar <double> bar2;
+  osm::ProgressBar <float> bar3;
+  auto bars = osm::MultiProgressBar( bar1, bar2, bar3 );
+ 
+  TEST_SUITE_BEGIN( "Main methods" );
+  
+  //====================================================
+  //     Testing "size" method
+  //====================================================
+  SUBCASE( "Testing the size method" )
    {
-    auto first_output_multi = [ &i ]( std::ostream& ){ bars.for_one( 0, osm::updater{}, i ); };
-    auto first_output_normal = [ &i ]( std::ostream& ){ bar1.update( i ); };
-
-    auto old_buffer = std::cout.rdbuf(nullptr);
-    first_output_multi( ss_multi );
-    first_output_normal( ss_normal );
-    std::cout.rdbuf(old_buffer);
-
-    CHECK_EQ( ss_multi.str(), ss_normal.str() );
-   }  
-
-  bar2.setMin( 1.0 );
-  bar2.setMax ( 10.0 );
-  bar2.setStyle( "loader", "#" );
-
-  for( double i = bar2.getMin(); i < bar2.getMax(); i += 0.1 ) 
-   {
-    auto first_output_multi = [ &i ]( std::ostream& ){ bars.for_one( 1, osm::updater{}, i ); };
-    auto first_output_normal = [ &i ]( std::ostream& ){ bar2.update( i ); };
-
-    auto old_buffer = std::cout.rdbuf(nullptr);
-    first_output_multi( ss_multi );
-    first_output_normal( ss_normal );
-    std::cout.rdbuf(old_buffer);
-
-    CHECK_EQ( ss_multi.str(), ss_normal.str() );
-   }  
-
-  bar3.setMin( 1.2f );
-  bar3.setMax ( 5.8f );
-  bar3.setStyle( "complete", "%", "#" );
-
-  for( float i = bar3.getMin(); i < bar3.getMax(); i += 0.1f ) 
-   {
-    auto first_output_multi = [ &i ]( std::ostream& ){ bars.for_one( 2, osm::updater{}, i ); };
-    auto first_output_normal = [ &i ]( std::ostream& ){ bar3.update( i ); };
-
-    auto old_buffer = std::cout.rdbuf(nullptr);
-    first_output_multi( ss_multi );
-    first_output_normal( ss_normal );
-    std::cout.rdbuf(old_buffer);
-
-    CHECK_EQ( ss_multi.str(), ss_normal.str() );
+    CHECK_EQ( bars.size(), 3 );
    }
- }
+  
+  //====================================================
+  //     Testing "operator()" redefinition
+  //====================================================
+  SUBCASE( "Testing the operator() redefinition in the updater struct" )
+   {
+    bar1.setMin( 0 );
+    bar1.setMax ( 60 );
+    bar1.setStyle( "indicator", "%" );
+  
+    static std::stringstream ss_multi, ss_normal;
+  
+    for( int i = bar1.getMin(); i < bar1.getMax(); i++ ) 
+     {
+      auto first_output_multi = [ &i, &bars ]( std::ostream& ){ bars.for_one( 0, osm::updater{}, i ); };
+      auto first_output_normal = [ &i, &bar1 ]( std::ostream& ){ bar1.update( i ); };
+  
+      auto old_buffer = std::cout.rdbuf(nullptr);
+      first_output_multi( ss_multi );
+      first_output_normal( ss_normal );
+      std::cout.rdbuf(old_buffer);
+  
+      CHECK_EQ( ss_multi.str(), ss_normal.str() );
+     }  
+  
+    bar2.setMin( 1.0 );
+    bar2.setMax ( 10.0 );
+    bar2.setStyle( "loader", "#" );
+  
+    for( double i = bar2.getMin(); i < bar2.getMax(); i += 0.1 ) 
+     {
+      auto first_output_multi = [ &i, &bars ]( std::ostream& ){ bars.for_one( 1, osm::updater{}, i ); };
+      auto first_output_normal = [ &i, &bar2 ]( std::ostream& ){ bar2.update( i ); };
+  
+      auto old_buffer = std::cout.rdbuf(nullptr);
+      first_output_multi( ss_multi );
+      first_output_normal( ss_normal );
+      std::cout.rdbuf(old_buffer);
+  
+      CHECK_EQ( ss_multi.str(), ss_normal.str() );
+     }  
+  
+    bar3.setMin( 1.2f );
+    bar3.setMax ( 5.8f );
+    bar3.setStyle( "complete", "%", "#" );
+  
+    for( float i = bar3.getMin(); i < bar3.getMax(); i += 0.1f ) 
+     {
+      auto first_output_multi = [ &i, &bars ]( std::ostream& ){ bars.for_one( 2, osm::updater{}, i ); };
+      auto first_output_normal = [ &i, &bar3 ]( std::ostream& ){ bar3.update( i ); };
+  
+      auto old_buffer = std::cout.rdbuf(nullptr);
+      first_output_multi( ss_multi );
+      first_output_normal( ss_normal );
+      std::cout.rdbuf(old_buffer);
+  
+      CHECK_EQ( ss_multi.str(), ss_normal.str() );
+     }
+   }
+ 
+  TEST_SUITE_END();
+ } 
