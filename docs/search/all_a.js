e43b4432905e9b8e74321553da56b44fc78d40fa
Carl Timmer <timmer@jlab.org>
2022-06-24 18:02:13 +0000

    Update docs

--- docs/search/all_a.js
+++ docs/search/all_a.js
@@ -5,6 +5,6 @@ var searchData=
   ['on_61',['ON',['../namespaceosm.html#a793613c2bce496156326ba85a53b6d13a90651ebea9a35ec4e018c8157492e17c',1,'osm']]],
   ['operator_28_29_62',['operator()',['../structosm_1_1updater.html#ae923dcfd5f29ccc59c843adfe8f18361',1,'osm::updater']]],
   ['operator_3c_3c_63',['operator&lt;&lt;',['../namespaceosm.html#af39d33e13c1d113bf8519fa8d316a920',1,'osm']]],
-  ['option_64',['OPTION',['../namespaceosm.html#a5b99231c2369e9c5ddda206ec220fb26',1,'osm']]],
+  ['option_64',['OPTION',['../namespaceosm.html#ab97e4347ae4e25d091a0b79b118e2a29',1,'osm']]],
   ['osm_65',['osm',['../namespaceosm.html',1,'']]]
 ];
--- docs/search/functions_9.js
+++ docs/search/functions_9.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['operator_28_29_180',['operator()',['../structosm_1_1updater.html#ae923dcfd5f29ccc59c843adfe8f18361',1,'osm::updater']]],
   ['operator_3c_3c_181',['operator&lt;&lt;',['../namespaceosm.html#af39d33e13c1d113bf8519fa8d316a920',1,'osm']]],
-  ['option_182',['OPTION',['../namespaceosm.html#a5b99231c2369e9c5ddda206ec220fb26',1,'osm']]]
+  ['option_182',['OPTION',['../namespaceosm.html#ab97e4347ae4e25d091a0b79b118e2a29',1,'osm']]]
 ];
