5964ddb4b4ec309d7619a7e7430de32088309d8c
Joel <brolgedu@gmail.com>
2022-10-05 15:00:05 -0700

    Added output redirection test -- Test cases will be added in the near future.

--- test/unit_tests/redirection/output_redirector.cpp
+++ test/unit_tests/redirection/output_redirector.cpp
@@ -1,3 +1,40 @@
-//
-// Created by Joel on 10/5/2022.
-//
+// My headers
+#include "../include/progressbar/progress_bar.hpp"
+#include "../include/redirection/output_redirector.hpp"
+
+// Extra headers
+#include <doctest/doctest.h>
+
+// STD headers
+#include <iostream>
+#include <thread>
+
+TEST_CASE( "Testing OutputRedirector class" )
+{
+  std::string filename = "output.txt";
+  osm::OutputRedirector redirector( filename );
+
+  // Normal percentage bar.
+  osm::ProgressBar<int> percentage_bar;
+  percentage_bar.setMin( 5 );
+  percentage_bar.setMax( 46 );
+  percentage_bar.setStyle( "indicator", "%" );
+
+  // Redirect output
+  redirector.begin();
+
+  std::cout << "This is a normal percentage bar: "
+            << "\n";
+  for( int i = percentage_bar.getMin(); i < percentage_bar.getMax(); i++ )
+  {
+    // Flush the buffer at the start of each loop
+    redirector.flush();
+
+    std::this_thread::sleep_for( std::chrono::milliseconds( 100 ) );
+    percentage_bar.update( i );
+    // Do some operations...
+  }
+
+  // Return output to std::cout
+  redirector.end();
+}
