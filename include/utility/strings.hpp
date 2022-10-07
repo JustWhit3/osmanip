de661d91ce1305d22a2443e4734a0f152a784953
Joel <brolgedu@gmail.com>
2022-10-06 18:45:27 -0700

    Updated documentation

--- include/utility/strings.hpp
+++ include/utility/strings.hpp
@@ -2,19 +2,38 @@
 //     File data
 //====================================================
 /**
- * @file strings.h
+ * @file strings.hpp
  * @author Joel Thomas (joelthomas.e@gmail.com)
  * @date 2022-10-5
  */
 
+//====================================================
+//     Preprocessor settings
+//====================================================
 #pragma once
+#ifndef OSMANIP_STRINGS_HPP
+#define OSMANIP_STRINGS_HPP
+#endif
 
+//====================================================
+//     Headers
+//====================================================
+
+// STD headers
 #include <string>
 
 namespace osm
 {
 
-  // Returns the index of the first alphabetic character in the string, starting from the given position.
+  // find_first_alpha
+  /**
+   * @brief Finds the index of the first alphabetic character in the string, starting from the given position.
+   *
+   * @param pos the starting position.
+   * @param string the string to be searched.
+   *
+   * @return size_t of the index if successful, otherwise std::string::npos.
+   */
   size_t find_first_alpha( size_t pos, const std::string & string )
   {
     for( size_t i = pos; i < string.size(); ++i )
@@ -27,7 +46,14 @@ namespace osm
     return std::string::npos;
   }
 
-  // Returns a formatted the string, effectively removing all escape sequences present in the string.
+  // get_formatted_string
+  /**
+   * @brief Gets a copy of the given string with all ANSI escape sequences removed.
+   *
+   * @param string the string to be formatted.
+   *
+   * @return std::string copy without ANSI escape sequences.
+   */
   std::string get_formatted_string( const std::string & string )
   {
     std::string result = string;
@@ -49,9 +75,14 @@ namespace osm
     return result;
   }
 
-  // Checks for an endline starting from the end of the given string and returns a string.
-  //  If no endline is found, a copy of the string is returned. Otherwise, a copy of the
-  //  given string is returned with the last line erased up to the first endline found.
+  // erase_last_line
+  /**
+   * @brief Checks for an endline starting from the end of the given string and returns a string. If no endline is found, a copy of the string is returned. Otherwise, a copy of the given string is returned with the last line erased up to the first endline found.
+   *
+   * @param string the string to be searched.
+   *
+   * @return std::string substring with the last line erased or a copy if an endline is not found.
+   */
   static std::string erase_last_line( const std::string & string )
   {
     if( auto it = string.rfind( '\n', string.size() - 1 ); it != std::string::npos )
