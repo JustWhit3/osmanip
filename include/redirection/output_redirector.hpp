304b910497be4ab5fd779dfc042db2e6bb42e152
Joel <brolgedu@gmail.com>
2022-10-12 11:35:30 -0700

    Added a reference counter and santiy checks to the class

--- include/redirection/output_redirector.hpp
+++ include/redirection/output_redirector.hpp
@@ -12,21 +12,27 @@
 //====================================================
 #pragma once
 #ifndef OSMANIP_OUTPUTREDIRECTOR_HPP
-#define OSMANIP_OUTPUTREDIRECTOR_HPP
-
+#  define OSMANIP_OUTPUTREDIRECTOR_HPP
 
 //====================================================
 //     Headers
 //====================================================
 
 // STD headers
-#include <mutex>
-#include <string>
-#include <fstream>
-#include <streambuf>
+#  include <mutex>
+#  include <string>
+#  include <fstream>
+#  include <streambuf>
+#  include <filesystem>
+#  include <atomic>
 
 namespace osm
 {
+  //====================================================
+  //     Aliases
+  //====================================================
+  namespace fs = std::filesystem;
+
   //====================================================
   //     Classes
   //====================================================
@@ -46,7 +52,7 @@ namespace osm
     //     Constructors
     //====================================================
     OutputRedirector();
-    explicit OutputRedirector( std::string & filename );
+    explicit OutputRedirector( std::string filename );
 
     //====================================================
     //     Destructor
@@ -57,11 +63,13 @@ namespace osm
     //     Setters
     //====================================================
     void setFilename( const std::string & filename );
+    //     void setFilepath( fs::path & path );
 
     //====================================================
     //     Getters
     //====================================================
     [[nodiscard]] std::string & getFilename();
+    [[nodiscard]] std::string & getFilepath();
 
     //====================================================
     //     Methods
@@ -71,12 +79,24 @@ namespace osm
     void flush();
     void touch();
 
+    //====================================================
+    //     Static public variable
+    //====================================================
+    static const std::string DEFAULT_FILE_DIR;
+    static const std::string DEFAULT_FILEPATH;
+    static const std::string DEFAULT_FILENAME;
+
     private:
     //====================================================
     //     Private attributes
     //====================================================
     std::mutex mutex_;
+    std::atomic<int32_t> ref_count;
+
+    std::string file_dir_;
     std::string filename_;
+    std::string filepath_;
+
     std::fstream fstream_;
     std::stringbuf * output_stringbuf_;
     std::streambuf * streambuf_backup_;
@@ -86,6 +106,13 @@ namespace osm
     //====================================================
     void clear_buffer();
     void redirect_output( std::string & filename );
+
+    // File
+    std::string read_file( const std::string & filename );
+    bool write_to_file( const std::string & filename, const std::string & out_string );
+
+    // Exceptions
+    void sanity_check( const std::string & func_name );
     void exception_file_not_found();
   };
 
