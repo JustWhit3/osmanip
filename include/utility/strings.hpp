0b113dcd667fa25238aca3b9ed46ab695545d461
Joel <brolgedu@gmail.com>
2022-11-04 15:38:48 -0700

    Added additional functions to handle ANSI escape sequences.

--- include/utility/strings.hpp
+++ include/utility/strings.hpp
@@ -20,83 +20,23 @@
 //====================================================
 
 // STD headers
-#include <string>
-#include <stddef.h>
-#include <cctype>
+#  include <string>
+
 namespace osm
- {
+{
+
   //====================================================
   //     Functions
   //====================================================
 
-  // find_first_alpha
-  /**
-   * @brief Finds the index of the first alphabetic character in the string, starting from the given position.
-   *
-   * @param pos the starting position.
-   * @param string the string to be searched.
-   *
-   * @return size_t of the index if successful, otherwise std::string::npos.
-   */
-  static size_t find_first_alpha( size_t pos, const std::string & string )
-   {
-    for( size_t i = pos; i < string.size(); ++i )
-     {
-      if( std::isalpha( static_cast<unsigned char>( string[ i ] ) ) )
-       {
-        return i;
-       }
-     }
-    return std::string::npos;
-   }
+  extern std::string get_formatted_from_ansi( const std::string & str, int * last_pos = nullptr, int * last_size = nullptr );
+  [[maybe_unused]] extern std::string trim_string( std::string & str );
+  [[maybe_unused]] extern size_t find_first_alpha( const std::string & str, size_t pos );
+  [[maybe_unused]] extern std::string get_ansi_csi_string( const std::string & str, size_t esc_pos );
+  [[maybe_unused]] extern int get_ansi_csi_number( const std::string & csi );
+  [[maybe_unused]] extern char get_ansi_csi_code( const std::string & csi );
+  [[maybe_unused]] extern void handle_csi( const std::string & csi_str, std::string & dst_str, int * dst_crsr_pos );
 
-  // get_formatted_string
-  /**
-   * @brief Gets a copy of the given string with all ANSI escape sequences removed.
-   *
-   * @param string the string to be formatted.
-   *
-   * @return std::string copy without ANSI escape sequences.
-   */
-  static std::string get_formatted_string( const std::string & string )
-   {
-    std::string result = string;
-
-    for( size_t i = 0; i < result.size(); ++i )
-     {
-      if( size_t esc_pos = result.find( "\u001b[", i ); esc_pos != std::string::npos )
-       {
-        size_t bracket_pos = esc_pos + 1;
-        if( size_t alpha_pos = find_first_alpha( bracket_pos + 1, result ); alpha_pos != std::string::npos )
-         {
-          // We want the range from the char after the bracket and the alpha inclusive, so we add one to each position
-          result.erase( esc_pos, alpha_pos - bracket_pos + 2 );
-          i = ( int )esc_pos - 1;
-         }
-       }
-     }
-
-    return result;
-   }
-
-  // erase_last_line
-  /**
-   * @brief Checks for an endline starting from the end of the given string and returns a string. If no endline is found, a copy of the string is returned. Otherwise, a copy of the given string is returned with the last line erased up to the first endline found.
-   *
-   * @param string the string to be searched.
-   *
-   * @return std::string substring with the last line erased or a copy if an endline is not found.
-   */
-  static std::string erase_last_line( const std::string & string )
-   {
-    if( auto it = string.rfind( '\n', string.size() - 1 ); it != std::string::npos )
-     {
-      std::string result = string;
-      result.erase( it + 1 );
-      return result;
-     }
-    return string;
-   }
- } 
+}      // namespace osm
 
 #endif
