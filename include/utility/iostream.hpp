1502b58ae6910e86919c271f29682d9f5964c97b
Joel <brolgedu@gmail.com>
2022-11-04 15:42:29 -0700

    Added global osm::cout and osm::redirout variables.

---
+++ include/utility/iostream.hpp
@@ -0,0 +1,41 @@
+//====================================================
+//     File data
+//====================================================
+/**
+ * @file iostream.hpp
+ * @author Joel Thomas (joelthomas.e@gmail.com)
+ * @date 2022-11-2
+ * @copyright Copyright (c) 2022 Gianluca Bianco under the MIT license.
+ */
+
+//====================================================
+//     Preprocessor settings
+//====================================================
+
+#pragma once
+#ifndef OSMANIP_COUT_INCLUDE_UTILITY_IOSTREAM_HPP
+#  define OSMANIP_COUT_INCLUDE_UTILITY_IOSTREAM_HPP
+
+//====================================================
+//     Headers
+//====================================================
+
+// My headers
+#include "../../include/utility/output_redirector.hpp"
+
+// STD headers
+#  include <ostream>
+
+namespace osm
+{
+
+  //====================================================
+  //     Global variables
+  //====================================================
+
+  extern std::ostream cout;              /// Linked to standard output
+  extern OutputRedirector redirout;      /// Linked to output redirection
+
+}      // namespace osm
+
+#endif      // OSMANIP_COUT_INCLUDE_UTILITY_IOSTREAM_HPP
