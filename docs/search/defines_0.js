b4ffedef428bbfaeb7630bd760fa044a00e00095
Carl Timmer <timmer@jlab.org>
2023-07-25 13:11:05 +0000

    Update docs

--- docs/search/defines_0.js
+++ docs/search/defines_0.js
@@ -1,18 +1,18 @@
 var searchData=
 [
-  ['osmanip_5fcanvas_5fhpp_385',['OSMANIP_CANVAS_HPP',['../canvas_8hpp.html#ac7cffc6a71ecc6f474383f9e559394f8',1,'canvas.hpp']]],
-  ['osmanip_5fcolsty_5fhpp_386',['OSMANIP_COLSTY_HPP',['../colsty_8hpp.html#ad88a3aaec6b45fcdc505b56cae2663de',1,'colsty.hpp']]],
-  ['osmanip_5fcommon_5fhpp_387',['OSMANIP_COMMON_HPP',['../common_8hpp.html#a83f7803d6d27eb0857cbf250dac3b3d0',1,'common.hpp']]],
-  ['osmanip_5fcout_5finclude_5futility_5fiostream_5fhpp_388',['OSMANIP_COUT_INCLUDE_UTILITY_IOSTREAM_HPP',['../iostream_8hpp.html#a4f8a2d7ce55d10e182afd55d2074c87f',1,'iostream.hpp']]],
-  ['osmanip_5fcursor_5fhpp_389',['OSMANIP_CURSOR_HPP',['../cursor_8hpp.html#a40b93387877caad93d9beb7f792a2f22',1,'cursor.hpp']]],
-  ['osmanip_5fdecorator_5fhpp_390',['OSMANIP_DECORATOR_HPP',['../decorator_8hpp.html#ad579cefe6922c3d5277a81d3df728846',1,'decorator.hpp']]],
-  ['osmanip_5fmain_5finclude_5futility_5fsstream_5fhpp_391',['OSMANIP_MAIN_INCLUDE_UTILITY_SSTREAM_HPP',['../sstream_8hpp.html#a19e7151aeb7be8d1ecc9da952b8b45a9',1,'sstream.hpp']]],
-  ['osmanip_5fmultiprogressbar_5fhpp_392',['OSMANIP_MULTIPROGRESSBAR_HPP',['../multi__progress__bar_8hpp.html#a8b9a6794e1b7a633023f36d2950737a8',1,'multi_progress_bar.hpp']]],
-  ['osmanip_5foptions_5fhpp_393',['OSMANIP_OPTIONS_HPP',['../options_8hpp.html#a155bd8f8d72e52395ee7ac841547a9ca',1,'options.hpp']]],
-  ['osmanip_5foutputredirector_5fhpp_394',['OSMANIP_OUTPUTREDIRECTOR_HPP',['../output__redirector_8hpp.html#ab291bb968d81d8cc5c62a0e1a1d8b7c0',1,'output_redirector.hpp']]],
-  ['osmanip_5fplot2dcanvas_395',['OSMANIP_PLOT2DCANVAS',['../plot__2D_8hpp.html#afad9ac49cb72f2fcf978b173c709b637',1,'plot_2D.hpp']]],
-  ['osmanip_5fprogressbar_5fhpp_396',['OSMANIP_PROGRESSBAR_HPP',['../progress__bar_8hpp.html#a80d24d062caf6e04e78b214bfbe2a260',1,'progress_bar.hpp']]],
-  ['osmanip_5fstrings_5fhpp_397',['OSMANIP_STRINGS_HPP',['../strings_8hpp.html#a0fbf4a9587e31737cf6b8a3c849ae980',1,'strings.hpp']]],
-  ['osmanip_5futility_5fgeneric_398',['OSMANIP_UTILITY_GENERIC',['../generic_8hpp.html#ab09c9ed198183388ee701017599cd819',1,'generic.hpp']]],
-  ['osmanip_5fwindows_5fhpp_399',['OSMANIP_WINDOWS_HPP',['../windows_8hpp.html#a2b6a8a6be19719c72e14df2268b36d94',1,'windows.hpp']]]
+  ['osmanip_5fgraphics_5fcanvas_5fhpp_385',['OSMANIP_GRAPHICS_CANVAS_HPP',['../canvas_8hpp.html#a561683f833542f870d2f37212793cab6',1,'canvas.hpp']]],
+  ['osmanip_5fgraphics_5fplot2dcanvas_5fhpp_386',['OSMANIP_GRAPHICS_PLOT2DCANVAS_HPP',['../plot__2D_8hpp.html#a65031a98b3ef2d2da76c46986046f2e8',1,'plot_2D.hpp']]],
+  ['osmanip_5fmanipulators_5fcolsty_5fhpp_387',['OSMANIP_MANIPULATORS_COLSTY_HPP',['../colsty_8hpp.html#a17622d4b559199212248ad7030f3e2e4',1,'colsty.hpp']]],
+  ['osmanip_5fmanipulators_5fcommon_5fhpp_388',['OSMANIP_MANIPULATORS_COMMON_HPP',['../common_8hpp.html#a39f617b2a40dc72ab8d246becd23ae42',1,'common.hpp']]],
+  ['osmanip_5fmanipulators_5fcursor_5fhpp_389',['OSMANIP_MANIPULATORS_CURSOR_HPP',['../cursor_8hpp.html#a763a5c5c89c8862038cb23bc2d0fe987',1,'cursor.hpp']]],
+  ['osmanip_5fmanipulators_5fdecorator_5fhpp_390',['OSMANIP_MANIPULATORS_DECORATOR_HPP',['../decorator_8hpp.html#a21e978964f28af1fd69347f84d9aff43',1,'decorator.hpp']]],
+  ['osmanip_5fprogressbar_5fmultiprogressbar_5fhpp_391',['OSMANIP_PROGRESSBAR_MULTIPROGRESSBAR_HPP',['../multi__progress__bar_8hpp.html#a4fc086c8519c8914a3f7fddbefaaa9a4',1,'multi_progress_bar.hpp']]],
+  ['osmanip_5fprogressbar_5fprogressbar_5fhpp_392',['OSMANIP_PROGRESSBAR_PROGRESSBAR_HPP',['../progress__bar_8hpp.html#a8e0812e3794b24df6cf3f7b8228bc6ee',1,'progress_bar.hpp']]],
+  ['osmanip_5futility_5fgeneric_5fhpp_393',['OSMANIP_UTILITY_GENERIC_HPP',['../generic_8hpp.html#a50b3592c7d0e1ddbf811e0b0724af7c5',1,'generic.hpp']]],
+  ['osmanip_5futility_5fiostream_5fhpp_394',['OSMANIP_UTILITY_IOSTREAM_HPP',['../iostream_8hpp.html#af720c40cbe5a9cfb8c5e0412310a268a',1,'iostream.hpp']]],
+  ['osmanip_5futility_5foptions_5fhpp_395',['OSMANIP_UTILITY_OPTIONS_HPP',['../options_8hpp.html#aabe64ab486fc770f8c098eab48451b3b',1,'options.hpp']]],
+  ['osmanip_5futility_5foutputredirector_5fhpp_396',['OSMANIP_UTILITY_OUTPUTREDIRECTOR_HPP',['../output__redirector_8hpp.html#a3623887d75ddfab0c46dbffc2e21bad2',1,'output_redirector.hpp']]],
+  ['osmanip_5futility_5fsstream_5fhpp_397',['OSMANIP_UTILITY_SSTREAM_HPP',['../sstream_8hpp.html#a802f9b00bea53848c074025148b0ad68',1,'sstream.hpp']]],
+  ['osmanip_5futility_5fstrings_5fhpp_398',['OSMANIP_UTILITY_STRINGS_HPP',['../strings_8hpp.html#a694a7ed8a09fff62544c727ca9ffdc8c',1,'strings.hpp']]],
+  ['osmanip_5futility_5fwindows_5fhpp_399',['OSMANIP_UTILITY_WINDOWS_HPP',['../windows_8hpp.html#aec172a48969186a4adc4a391b50aa60a',1,'windows.hpp']]]
 ];
